02e129dc289070332400056782fa3f6d86f4f974
Jay Karimi <jkarimi91@gmail.com>
2017-11-01 13:41:27 -0700

    Make vocab class more flexible

--- vocab.py
+++ vocab.py
@@ -1,38 +1,43 @@
-from collections import Counter
-from itertools import chain
-
-PAD_LABEL = 0
-UNK_LABEL = 1
-SOS_LABEL = 2
-EOS_LABEL = 3
-NUM_SPECIAL_LABELS = 4
-
-
 class Vocab(object):
-    def __init__(self, data, max_vocab_size=None):
-        if max_vocab_size is not None:
-            max_vocab_size -= NUM_SPECIAL_LABELS
+    PAD_LABEL = 0
+    UNK_LABEL = 1
+    SOS_LABEL = 2
+    EOS_LABEL = 3
 
-        tokens = chain(*chain(*data))
-        most_common = [token for token, _ in Counter(tokens).most_common(max_vocab_size)]
-        self.token_to_label = {token: i + NUM_SPECIAL_LABELS for i, token in enumerate(most_common)}
-        self.label_to_token = {label: token for token, label in self.token_to_label.iteritems()}
+    PAD_TOKEN = 'pad>'
+    UNK_TOKEN = '<unk>'
+    SOS_TOKEN = '<sos>'
+    EOS_TOKEN = '<eos>'
+
+    def __init__(self):
+        self.label_to_token = {
+            Vocab.PAD_LABEL: Vocab.PAD_TOKEN,
+            Vocab.UNK_LABEL: Vocab.UNK_TOKEN,
+            Vocab.SOS_LABEL: Vocab.SOS_TOKEN,
+            Vocab.EOS_LABEL: Vocab.EOS_TOKEN,
+        }
+        self.token_to_label = {token: label for label, token in self.label_to_token.iteritems()}
+        self.vocab_size = len(self.label_to_token)
 
     def __len__(self):
-        return NUM_SPECIAL_LABELS + len(self.token_to_label)
+        return self.vocab_size
 
-    def label_encode(self, tokens, use_sos=False):
-        labels = [self.token_to_label[t] if t in self.token_to_label else UNK_LABEL for t in tokens]
-        labels.append(EOS_LABEL)
-        if use_sos:
-            labels.insert(0, SOS_LABEL)
+    def add_token(self, token):
+        if token not in self.token_to_label:
+            label = len(self)
+            self.token_to_label[token] = label
+            self.label_to_token[label] = token
+            self.vocab_size += 1
+
+    def label_encode(self, tokens):
+        labels = []
+        for t in tokens:
+            if t in self.token_to_label:
+                l = self.token_to_label[t]
+            else:
+                l = Vocab.UNK_LABEL
+            labels.append(l)
         return labels
 
     def label_decode(self, labels):
-        tokens = []
-        for l in labels:
-            if l in self.label_to_token:
-                tokens.append(self.label_to_token[l])
-            elif l == UNK_LABEL:
-                tokens.append('<unk>')
-        return tokens
+        return [self.label_to_token[l] for l in labels]
