48ee2bf12e369ba8cefd5e4e15d3f2a3fade7cde
Jay Karimi <jkarimi91@gmail.com>
2017-11-01 13:53:55 -0700

    Fix chat and pad seq fxns

--- utils.py
+++ utils.py
@@ -1,47 +1,36 @@
 import random
 from itertools import chain
 
-from vocab import PAD_LABEL
+import torch
+from torch.autograd import Variable
+
+from models import init_hidden
+from vocab import Vocab
 
 
-# TODO: update chat function
-# def chat(model):
-#     vocab = load_vocab(VOCAB_FILE_NAME)
-#     inverted_vocab = {i: w for w, i in vocab.iteritems()}
-#
-#     batch_size = 1
-#     decoder_hidden = init_hidden(model.num_layers, batch_size, model.hidden_size)
-#     decoder_output = Variable(torch.LongTensor([tokens_to_seq([GO_TOKEN], vocab)]), volatile=True).cuda()
-#
-#     while True:
-#         question = raw_input('User: ')
-#         question = [tokens_to_seq(tokenize(question), vocab)]
-#         question = Variable(torch.LongTensor(question), volatile=True).cuda()
-#         _, encoder_hidden = model.encoder(question)
-#
-#         response = []
-#
-#         eos = False
-#         while not eos:
-#             decoder_output, decoder_hidden = model.decoder(decoder_output, encoder_hidden, decoder_hidden)
-#             top_i = decoder_output[0, 0].data.topk(1)[1][0]
-#
-#             decoder_output = Variable(torch.LongTensor([[top_i]]), volatile=True).cuda()
-#             word = seq_to_tokens([top_i], inverted_vocab)[0]
-#
-#             if word == EOS_TOKEN:
-#                 eos = True
-#             else:
-#                 response.append(word)
-#
-#         print 'Chatbot: {}'.format(' '.join(response))
+def chat(input_text, tokenizer, model, vocab, max_output_len=25):
+    input_text = vocab.label_encode(tokenizer(input_text))
+    input_text = Variable(torch.LongTensor([input_text]), volatile=True).cuda()
+    output = [vocab.SOS_LABEL]
+
+    _, encoder_hidden = model.encoder(input_text)
+    hidden = init_hidden(model.num_layers, 1, model.hidden_size)
+
+    prediction = vocab.SOS_LABEL
+    while len(output) < max_output_len and prediction != vocab.EOS_LABEL:
+        prediction = Variable(torch.LongTensor([[prediction]]), volatile=True).cuda()
+        prediction, _ = model.decoder(prediction, encoder_hidden, hidden)
+        prediction = prediction.data.topk(1)[1][0, 0, 0]
+        output.append(prediction)
+
+    return ' '.join(vocab.label_decode(output))
 
 
 def pad_seqs(seqs):
     max_seq_len = max(len(s) for s in seqs)
     result = []
     for s in seqs:
-        padding = [PAD_LABEL] * (max_seq_len - len(s))
+        padding = [Vocab.PAD_LABEL] * (max_seq_len - len(s))
         result.append(s + padding)
     return result
 
@@ -67,7 +56,7 @@ def load_data(encoding, file_path):
         file_data = [line.decode(encoding).split() for line in f]
 
     output_data = []
-    for i in xrange(len(file_data) - 1):
+    for i in xrange(0, len(file_data), 2):
         qa_pair = (file_data[i], file_data[i + 1])
         output_data.append(qa_pair)
     return output_data
